a97832a58ec9f18db0e3958a1005133b18110f57
Marco Benzi <marco.benzi@alumnos.usm.cl>
2014-10-06 04:38:13 -0300

    Refactored file writing code, added stream nullness checks, runs now on Linux

--- src/csa.c
+++ src/csa.c
@@ -9,13 +9,13 @@
 /* OS Detection*/
 #ifdef _WIN32
 	const char* OS_VERSION="Win32";
-	char* PLAY_COMMAND = "..\\win32\\sox\\sox -r 20000 -b 16 -L -e signed-integer -t raw ";
+	char* PLAY_COMMAND = "..\\win32\\sox\\sox -r 20000 -b 16 -L -e signed-integer -t raw %s -d";
 #elif __APPLE__
     const char* OS_VERSION="Apple";
 #error NOT SUPORTED YET
 #elif __linux
     const char* OS_VERSION="Linux";
-	char* PLAY_COMMAND = "aplay --format=S16_LE -t raw ";
+	char* PLAY_COMMAND = "aplay --format=S16_LE -t raw %s";
 #endif
 
 void check_data(short int*, short int*, int);
@@ -37,8 +37,8 @@ int main(int argc, const char* argv[]){
 		/* Check de archivo valido y se obtiene el tamanio */
 		struct stat st;
 		int filesize;
-		char g_file[50]="g_", r_file[50]="r_";
-		
+		// MBT: Si prepones una extension al nombre del archivo, cuando uses un archivo dentro de una carpeta GG	
+		char g_file[100], r_file[100];
 		if(stat(argv[1], &st) != 0){
 			printf("Invalid file\n");
 			exit(1);
@@ -49,8 +49,8 @@ int main(int argc, const char* argv[]){
 			if( filesize%2 != 0 ){
 				filesize--;
 			}
-			strcat(g_file, argv[1]);
-			strcat(r_file, argv[1]);
+			sprintf(g_file,"%s%s",argv[1],"_g");
+			sprintf(r_file,"%s%s",argv[1],"_r");
 		}
 		
 		/* Se concatena el comando */
@@ -60,18 +60,10 @@ int main(int argc, const char* argv[]){
 		char play_command[CMD_LENGTH];
 		char play_command_g[CMD_LENGTH];
 		char play_command_r[CMD_LENGTH];
-		strcpy(play_command, PLAY_COMMAND);
-		strcat(play_command, argv[1]);
-		strcpy(play_command_g, PLAY_COMMAND);
-		strcat(play_command_g, g_file);
-		strcpy(play_command_r, PLAY_COMMAND);
-		strcat(play_command_r, r_file);
-		
-		if(strcmp(OS_VERSION,"Win32") == 0){
-			strcat(play_command, " -d");
-			strcat(play_command_g, " -d");
-			strcat(play_command_r, " -d");
-		}
+		// MBT: Para ordernar aplique sprintf en vez de strcpy y strcat
+		sprintf(play_command, PLAY_COMMAND, argv[1]);		
+		sprintf(play_command_g, PLAY_COMMAND, argv[1]);		
+		sprintf(play_command_r, PLAY_COMMAND, argv[1]);		
 		
 		/* Se muestra el comando y el tamanio del archivo */
 		printf("Play command: %s\n", play_command);
@@ -84,8 +76,6 @@ int main(int argc, const char* argv[]){
 		
 		/* Abertura de archivos, en modo binario, lectura y escritura segun corresponda */
 		FILE* input_file = fopen(argv[1], "rb");
-		//FILE* gain_file = fopen(g_file, "wb");
-		//FILE* rest_file = fopen(r_file, "wb");
 		
 		fread(orig_data, sizeof(short), filesize/2, input_file);
 		fclose(input_file);
@@ -110,15 +100,25 @@ int main(int argc, const char* argv[]){
 			//printf("%d.", gain_data[i]);
 		}
 		/* Se guardan los datos calculados en el archivo de ganancias */
-		//fwrite(gain_data, sizeof(short), filesize/2, gain_file);
-		//fclose(gain_file);
+		FILE* gain_file = fopen(g_file, "wb");
+		if (gain_file == NULL)  {
+			fprintf(stderr,"Error: couldn't create file %s.\n", g_file); 
+			abort();
+		}
+		fwrite(gain_data, sizeof(short), filesize/2, gain_file);
+		fclose(gain_file);
 		
 		/* Se Analiza el archivo con ganancia */
-		//check_data(gain_data, rest_data, filesize/2);
+		check_data(gain_data, rest_data, filesize/2);
 		
 		/* Se guarda la data restaurada en el archivo correspondiente */
-		//fwrite(rest_data, sizeof(short), filesize/2, rest_file);
-		//fclose(rest_file);
+		FILE* rest_file = fopen(r_file, "wb");
+		if (rest_file == NULL)  {
+			fprintf(stderr,"Error: couldn't create file %s.\n", r_file); 
+			abort();
+		}
+		fwrite(rest_data, sizeof(short), filesize/2, rest_file);
+		fclose(rest_file);
 		
 		/* Se reproduce el archivo de audio de entrada, amplificado y restaurado */
 		FILE* stream = popen(play_command, "r");
@@ -132,5 +132,5 @@ int main(int argc, const char* argv[]){
 }
 
 void check_data(short int *gain_data, short int *rest_data, int data_length){
-	printf("Archivo restaurado.");
+	printf("Archivo restaurado.\n");
 }
